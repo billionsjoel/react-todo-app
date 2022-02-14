75c1d1b052d5d17d5a3b3e6e9778e1e5109f7c9a
Billions Joel <joelatugonza@gmail.com>
2022-02-14 11:06:10 +0300

    Add component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoContainer from './components/TodoContainer';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header"></header>
+			<TodoContainer />
 		</div>
 	);
 }
