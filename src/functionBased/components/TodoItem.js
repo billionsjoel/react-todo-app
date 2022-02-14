d349dedad9a92b00e19b15e812c177942570c230
Billions Joel <joelatugonza@gmail.com>
2022-02-14 14:44:13 +0300

    Install uuid and fix linter

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,70 +1,73 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React, { useState, useEffect } from 'react';
-import styles from './TodoItem.module.css';
 import { FaTrash } from 'react-icons/fa';
+import styles from './TodoItem.module.css';
 
 const TodoItem = (props) => {
-	const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(false);
 
-	useEffect(() => {
-		return () => {
-			console.log('Cleaning up...');
-		};
-	}, []);
+  useEffect(() => () => {
+    console.log('Cleaning up...');
+  }, []);
 
-	const handleEditing = () => {
-		setEditing(true);
-	};
+  const handleEditing = () => {
+    setEditing(true);
+  };
 
-	const handleUpdatedDone = (event) => {
-		if (event.key === 'Enter') {
-			setEditing(false);
-		}
-	};
+  const handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      setEditing(false);
+    }
+  };
 
-	const completedStyle = {
-		fontStyle: 'italic',
-		color: '#595959',
-		opacity: 0.4,
-		textDecoration: 'line-through',
-	};
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
 
-	const { completed, id, title } = props.todo;
+  const { completed, id, title } = props.todo;
 
-	let viewMode = {};
-	let editMode = {};
+  const viewMode = {};
+  const editMode = {};
 
-	if (editing) {
-		viewMode.display = 'none';
-	} else {
-		editMode.display = 'none';
-	}
+  if (editing) {
+    viewMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
 
-	return (
-		<li className={styles.item}>
-			<div onDoubleClick={handleEditing} style={viewMode}>
-				<input
-					type="checkbox"
-					className={styles.checkbox}
-					checked={completed}
-					onChange={() => props.handleChangeProps(id)}
-				/>
-				<button onClick={() => props.deleteTodoProps(id)}>
-					<FaTrash style={{ color: 'orangered', fontSize: '16px' }} />
-				</button>
-				<span style={completed ? completedStyle : null}>{title}</span>
-			</div>
-			<input
-				type="text"
-				style={editMode}
-				className={styles.textInput}
-				value={title}
-				onChange={(e) => {
-					props.setUpdate(e.target.value, id);
-				}}
-				onKeyDown={handleUpdatedDone}
-			/>
-		</li>
-	);
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}>
+          <FaTrash style={{ color: 'orangered', fontSize: '16px' }} />
+        </button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={(e) => {
+				  props.setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
 };
 
 export default TodoItem;
