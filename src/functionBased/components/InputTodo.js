854c568576795a0197504ed5106f343311563712
Billions Joel <joelatugonza@gmail.com>
2022-02-14 14:46:56 +0300

    Fix prop type linter

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
