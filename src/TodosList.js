a7c6000d0a123021c5778833da0d600a993f25a9
Billions Joel <joelatugonza@gmail.com>
2022-02-14 11:55:59 +0300

    Create todo component

---
+++ src/TodosList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TodoList() {
+	return <div>TodoList</div>;
+}
+
+export default TodoList;
--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,9 +1,34 @@
-function TodoContainer() {
-	return (
-		<div>
-			<input type="checkbox" checked="{true}" />
-		</div>
-	);
+import React from 'react';
+import TodosList from '../TodosList';
+
+class TodoContainer extends React.Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Setup development environment',
+				completed: true,
+			},
+			{
+				id: 2,
+				title: 'Develop website and add content',
+				completed: false,
+			},
+			{
+				id: 3,
+				title: 'Deploy to live server',
+				completed: false,
+			},
+		],
+	};
+
+	render() {
+		return (
+			<div>
+				<TodosList todos={this.state.todos} />
+			</div>
+		);
+	}
 }
 
 export default TodoContainer;
