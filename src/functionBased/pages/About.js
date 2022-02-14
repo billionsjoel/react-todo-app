9759b0cd65ca166fd6cd48af57575144b506c1dd
Billions Joel <joelatugonza@gmail.com>
2022-02-14 13:28:02 +0300

    Use normal routing

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Link, useMatch, Route } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import SinglePage from './SinglePage';
 
 const About = () => {
-	const { url, path } = useMatch();
 	return (
 		<div className="about__content">
 			<ul className="about__list">
 				<li>
-					<Link to={`${url}/about-app`}>About App</Link>
+					<Link to={'/about-app'}>About App</Link>
 				</li>
 				<li>
-					<Link to={`${url}/about-author`}>About Author</Link>
+					<Link to={'/about-author'}>About Author</Link>
 				</li>
 			</ul>
-			<Route path={`${path}/:slug`}>
-				<SinglePage />
-			</Route>
+			<Routes>
+				<Route path={'/:slug'} element={<SinglePage />} />
+			</Routes>
 		</div>
 	);
 };
